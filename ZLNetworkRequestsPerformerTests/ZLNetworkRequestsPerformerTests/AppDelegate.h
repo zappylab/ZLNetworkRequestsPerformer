a217a3fd9ee760642d63ad7f374265bbb75d987d
Ilya Dyakonov <iluskadyakonov@gmail.com>
2014-04-29 13:38:11 +0200

    blank project

---
+++ ZLNetworkRequestsPerformerTests/ZLNetworkRequestsPerformerTests/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ZLNetworkRequestsPerformerTests
+//
+//  Created by Ilya Dyakonov on 29/04/14.
+//  Copyright (c) 2014 zappylab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ZLNetworkRequestsPerformerTests/ZLNetworkRequestsPerformerTests/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ZLNetworkRequestsPerformerTests
+//
+//  Created by Ilya Dyakonov on 29/04/14.
+//  Copyright (c) 2014 zappylab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
